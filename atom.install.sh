012ec65737a00fb06c0d8621ca9d413e269abfdc
Pavel Štětina <pavel.stetina@stringdata.cz>
2019-10-08 11:15:12 +0200

    Adds empty line for better readability

--- atom.install.sh
+++ atom.install.sh
@@ -1,4 +1,5 @@
 # PHP
 apm install atom-autocomplete-php atom-debug-ui atom-ide-ui ide-php linter-php php-composer-completion php-debug php-hyperclick
+
 # JSON
 apm install linter-json-lint pretty-json ide-json

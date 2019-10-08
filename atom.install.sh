4a338f4ff929b2637ba6c3d948daef27e5a99a3e
Pavel Štětina <pavel.stetina@stringdata.cz>
2019-10-08 10:54:40 +0200

    Install Ubuntu packages and snaps Install Atom.ide packages

---
+++ atom.install.sh
@@ -0,0 +1,4 @@
+# PHP
+apm install atom-autocomplete-php atom-debug-ui atom-ide-ui ide-php linter-php php-composer-completion php-debug php-hyperclick
+# JSON
+apm install linter-json-lint pretty-json ide-json
---
+++ ubuntu.install.sh
@@ -0,0 +1,17 @@
+# Encrypted disk and HOME
+apt-get install -y cryptsetup ecryptfs-utils
+
+# Install system monitor wia Chrome
+apt-get install -y chrome-gnome-shell #gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0
+# Visit https://extensions.gnome.org/extension/945/cpu-power-manager/
+# Visit https://extensions.gnome.org/extension/120/system-monitor/
+
+# Work
+apt-get install -y git php-cli vagrant virtualbox virtualbox-guest-additions-iso
+snap install atom --classic
+
+# Browsers
+snap install opera
+
+# Net
+apt-get install -y wakeonlan

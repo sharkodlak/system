3ee54eed9e0f163ce69c8c4530f1245bfda85a88
Pavel Štětina <pavel.stetina@stringdata.cz>
2019-10-30 18:15:48 +0100

    Add gnome shell extensions dependencies

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -2,7 +2,7 @@
 apt-get install -y cryptsetup ecryptfs-utils
 
 # Install system monitor wia Chrome
-apt-get install -y chrome-gnome-shell #gir1.2-gtop-2.0 gir1.2-networkmanager-1.0 gir1.2-clutter-1.0
+apt-get install -y chrome-gnome-shell gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
 # Visit https://extensions.gnome.org/extension/945/cpu-power-manager/
 # Visit https://extensions.gnome.org/extension/120/system-monitor/
 

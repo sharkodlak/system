cc130b5da7a4b451f7f103ad8f8cb5e0083043bb
Pavel Štětina <github@shark.kom.cz>
2019-11-09 15:29:52 +0100

    Add more packages for common usage. Mowe work packages to work.install.sh.

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -6,13 +6,18 @@ apt-get install -y chrome-gnome-shell gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutt
 # Visit https://extensions.gnome.org/extension/945/cpu-power-manager/
 # Visit https://extensions.gnome.org/extension/120/system-monitor/
 
-# Work
-apt-get install -y git gitk php-cli vagrant virtualbox virtualbox-guest-additions-iso
-snap install atom --classic
-
 # Browsers
-apt-get install profile-sync-daemon
-snap install opera
+apt-get install -y profile-sync-daemon adobe-flashplugin
+snap install opera # better is download Opera package and install it
 
 # Net
-apt-get install -y wakeonlan
+apt-get install -y net-tools pidgin wakeonlan
+
+# Video
+apt-get install -y mplayer smplayer
+
+# Wine
+apt-get install -y wine-stable winbind
+
+# Other
+apt-get install -y gparted htop i7z stellarium
---
+++ work.install.sh
@@ -0,0 +1,5 @@
+# Work
+apt-get install -y default-jdk git gitk php-cli vagrant virtualbox virtualbox-guest-additions-iso # dnsmasq wireshark
+snap install atom --classic
+
+. ./atom.install.sh

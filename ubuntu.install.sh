bb90911f12023a135fba032236aea811d9e1837f
Pavel Štětina <github@shark.kom.cz>
2021-01-20 22:29:15 +0100

    Drop old programs

--- ubuntu.install.sh
+++ ubuntu.install.sh
@@ -3,15 +3,16 @@ apt-get install -y cryptsetup ecryptfs-utils
 
 # Install system monitor wia Chrome
 apt-get install -y chrome-gnome-shell gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0
-# Visit https://extensions.gnome.org/extension/945/cpu-power-manager/
+# Visit https://extensions.gnome.org/extension/1082/cpufreq/
 # Visit https://extensions.gnome.org/extension/120/system-monitor/
+# Visit https://extensions.gnome.org/extension/2986/runcat/
 
 # Browsers
-apt-get install -y profile-sync-daemon adobe-flashplugin
-snap install opera # better is download Opera package and install it
+# apt-get install -y profile-sync-daemon adobe-flashplugin
+snap install opera # Opera package has problems with HTML5 video
 
 # Net
-apt-get install -y net-tools pidgin wakeonlan
+apt-get install -y net-tools wakeonlan
 
 # Video
 apt-get install -y mplayer smplayer
@@ -20,4 +21,5 @@ apt-get install -y mplayer smplayer
 apt-get install -y wine-stable winbind
 
 # Other
-apt-get install -y gparted htop i7z stellarium
+apt-get install -y gparted htop
+

fa64619dddab05402f1a30a92535fe4fd5e1e43a
Pavel Štětina <github@shark.kom.cz>
2021-01-21 09:56:54 +0100

    Update work.install.sh

--- work.install.sh
+++ work.install.sh
@@ -3,3 +3,7 @@ apt-get install -y default-jdk git gitk php-cli vagrant virtualbox virtualbox-gu
 snap install atom --classic
 
 . ./atom.install.sh
+
+
+# CNC
+apt install -y network-manager-openconnect network-manager-openconnect-gnome openconnect

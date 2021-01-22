506c38c430b6d8cd827542f3df51cd4ca9557b65
Pavel Stetina <pavel.stetina@devcity.tech>
2021-01-22 09:52:50 +0100

    Add Docker to work installations.

--- work.install.sh
+++ work.install.sh
@@ -9,3 +9,13 @@ snap install atom --classic
 
 # CNC
 apt install -y dnsmasq network-manager-openconnect network-manager-openconnect-gnome openconnect
+
+# Docker
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get update
+apt-get install -y docker-ce docker-ce-cli containerd.io
